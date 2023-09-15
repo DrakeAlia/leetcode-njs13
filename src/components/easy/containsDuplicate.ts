44cbce0bed5c9e912f2e47edec42c5cf913474bc
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-09-15 16:06:15 -0700

    add another solution for contains duplicate problem

---
+++ src/components/easy/containsDuplicate.ts
@@ -0,0 +1,76 @@
+// Contains Duplicate
+// https://leetcode.com/problems/contains-duplicate/
+
+// Given an integer array nums, return true if any value appears at least twice in the array,
+// and return false if every element is distinct.
+
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: true
+
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: false
+
+// Example 3:
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+// Constraints:
+// * 1 <= nums.length <= 105
+// * -109 <= nums[i] <= 109
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+// Solution using a set (hash table) and a loop (Fast runtime and low memory usage)
+var containsDuplicate = function (nums: any) {
+  // Set object lets you store unique values of any
+  // type, whether primitive values or object references.
+  // create a set to store the elements of the array
+  let set = new Set();
+  // loop through the array
+  for (let i = 0; i < nums.length; i++) {
+    // if the set has the current element
+    if (set.has(nums[i])) {
+      // return true
+      return true;
+    } else {
+      // add the current element to the set
+      set.add(nums[i]);
+    }
+  }
+  // return false
+  return false;
+};
+
+// Time Complexity: O(n) because we loop through the array once
+// Space Complexity: O(n) because we create a set to store the elements of the array
+
+// Tests cases
+console.log(containsDuplicate([1, 2, 3, 1])); // true
+console.log(containsDuplicate([1, 2, 3, 4])); // false
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // true
+
+// Alternative solution using a loop and sorting the array (Slow runtime)
+var containsDuplicate2 = function (nums: any) {
+  // The sort() method sorts the elements of an array
+  // in place and returns the sorted array.
+  // sort the array
+  nums.sort((a: any, b: any) => a - b);
+  // iterate over the sorted array
+  for (let i = 0; i < nums.length; i++) {
+    // if the current element is equal to the next element
+    if (nums[i] === nums[i + 1]) {
+      // return true
+      return true;
+    }
+  }
+  // return false
+  return false;
+};
+
+// Time Complexity: O(n log n) because we sort the array
+// Space Complexity: O(1) because we don't create any additional data structures
