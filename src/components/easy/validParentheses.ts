2cf2918e0600e9fccfe796448ce151edc932c6b6
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-09-14 17:18:10 -0700

    added another solution for validParentheses

--- src/components/easy/validParentheses.ts
+++ src/components/easy/validParentheses.ts
@@ -66,15 +66,53 @@ var isValid = function (s: string): boolean {
   return true;
 };
 
+// Test cases
+const result = isValid("()");
+console.log("Expected result: ", true);
+console.log("result:", result);
+
+const result2 = isValid("()[]{}");
+console.log("Expected result: ", true);
+console.log("result:", result2);
+
+const result3 = isValid("(]");
+console.log("Expected result: ", false);
+console.log("result:", result3);
+
 // Time: O(n) because we loop through the string once and the stack once
 // Space: O(n) because we create a stack and a map that are both the size of the string
 
-// Test cases
-const s1 = "()";
-console.log(isValid(s1)); // true
-
-
 // Another solution:
 var isValid2 = function (s: string | any[]) {
+  // create a map of the valid parentheses pairs
+  const map = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+  // Create a stack with a empty array as its value
+  const stack = [];
+  // Create a for loop, initialize the index with a value of zero,
+  // check if the index is less than the length of our string,
+  // and increment index by one once the code block finishes executing
+  for (let i = 0; i < s.length; i++) {
+    // Store the current character in a variabl
+    const char = s[i];
+    // Our if condition is going to check if the key value pair exists
+    if (map[char]) {
+      // If it does we going to push the value to our stack
+      stack.push(map[char]);
+      // else if the current character does not equal the element that we pop off our stack,
+    } else if (char !== stack.pop()) {
+      // We return false
+      return false;
+    }
+  }
+  // Once we exit our for loop, we going to check if the stack length is
+  // zero because if we have a empty stack that means were have a valid
+  // input string so we return it
+  stack.length === 0;
+};
 
-}
+// Time: O(n) because we loop through the string once and the stack once
+// Space: O(n) because we create a stack and a map that are both the size of the string
