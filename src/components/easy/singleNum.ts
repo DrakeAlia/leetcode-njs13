8334c507d97cc7e4faa24765ae8389aa949f138b
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-09-19 15:21:35 -0700

    found a solution for singleNum

--- src/components/easy/singleNum.ts
+++ src/components/easy/singleNum.ts
@@ -1,10 +1,9 @@
 // Single Number
 // https://leetcode.com/problems/single-number/
 
-
-// Given a non-empty array of integers nums, every element appears twice except 
+// Given a non-empty array of integers nums, every element appears twice except
 // for one. Find that single one.
-// You must implement a solution with a linear runtime complexity 
+// You must implement a solution with a linear runtime complexity
 // and use only constant extra space.
 
 // Example 1:
@@ -24,9 +23,6 @@
 // * -3 * 104 <= nums[i] <= 3 * 104
 // * Each element in the array appears twice except for one element which appears only once.
 
-// Tests cases:
-// console.log(singleNumber([2, 2, 1])); // 1
-
 /**
  * @param {number[]} nums
  * @return {number}
@@ -34,5 +30,29 @@
 
 // solution with a linear runtime complexity and constant extra space
 var singleNumber = function (nums: any) {
-
+  // create a variable to store the result
+  let result = 0;
+  // loop through the array
+  for (let i = 0; i < nums.length; i++) {
+    // Bitwise XOR operator (^) returns a 1 in each bit position for which
+    // the corresponding bits of either but not both operands are 1s.
+    // if the result is 0, it means that the current element is repeated
+    // if the result is not 0, it means that the current element is not repeated
+    result ^= nums[i];
+  }
+  // return the result
+  return result;
 };
+
+// Time Complexity: O(n) because we loop through the array once
+// Space Complexity: O(1) because we don't create any new data structures
+
+// Tests cases:
+// console.log(singleNumber([2, 2, 1])); // 1
+// console.log(singleNumber([4, 1, 2, 1, 2])); // 4
+// console.log(singleNumber([1])); // 1
+// console.log(singleNumber([1, 2, 3, 4, 5, 1, 2, 3, 4])); // 5
+
+// The bitwise XOR (^) operator returns a number or BigInt whose binary
+// representation has a 1 in each bit position for which the
+// corresponding bits of either but not both operands are 1.
